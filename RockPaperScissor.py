8996f6a1c17a3e92c6a4756237b9d5bca8b4e98c
Scampered <80023991+Scampered@users.noreply.github.com>
2021-11-19 18:18:36 +0300

    Add files via upload
    Main game file

---
+++ RockPaperScissor.py
@@ -0,0 +1,454 @@
+import random
+import time
+from typing import Any
+
+player_choice = input("(R)ock (P)aper or (S)cissors?: ")
+bot_choices = ['R', 'P', 'S']
+print(f"RPS Bot chooses:")
+bot_decision = random.choice(bot_choices)
+time.sleep(0.5)
+score = 0
+botpoints = 0
+
+if player_choice.lower == bot_decision:
+    print(f"Both selected {bot_decision}")
+
+elif player_choice == "R":
+    if bot_decision == "P":
+        print(f"Bot chose {bot_decision}")
+        print("You lost 1 point")
+        score = score - 1
+        botpoints = botpoints + 1
+    elif bot_decision == "S":
+        print(f"Bot chose {bot_decision}")
+        print("You gained 1 point!")
+        score = score + 1
+        botpoints = botpoints - 1
+elif player_choice == "P":
+    if bot_decision == "S":
+        print(f"Bot chose {bot_decision}")
+        print("You lost 1 point")
+        score = score - 1
+        botpoints = botpoints + 1
+    elif bot_decision == "R":
+        print(f"Bot chose {bot_decision}")
+        print("You gained 1 point!")
+        score = score + 1
+        botpoints = botpoints - 1
+elif player_choice == "S":
+    if bot_decision == "R":
+        print("You lost 1 point")
+        score = score - 1
+        botpoints = botpoints + 1
+    elif bot_decision == "P":
+        print(f"Bot chose {bot_decision}")
+        print("You gained 1 point!")
+        score = score + 1
+        botpoints = botpoints - 1
+
+print(f"Your current score: {score}")
+
+player_choice = input("(R)ock (P)aper or (S)cissors?: ")
+bot_choices = ['R', 'P', 'S']
+print(f"RPS Bot chooses:")
+bot_decision = random.choice(bot_choices)
+time.sleep(0.5)
+
+if player_choice.lower == bot_decision:
+    print(f"Both selected {bot_decision}")
+
+elif player_choice == "R":
+    if bot_decision == "P":
+        print(f"Bot chose {bot_decision}")
+        print("You lost 1 point")
+        score = score - 1
+        botpoints = botpoints + 1
+    elif bot_decision == "S":
+        print(f"Bot chose {bot_decision}")
+        print("You gained 1 point!")
+        score = score + 1
+        botpoints = botpoints - 1
+elif player_choice == "P":
+    if bot_decision == "S":
+        print(f"Bot chose {bot_decision}")
+        print("You lost 1 point")
+        score = score - 1
+        botpoints = botpoints + 1
+    elif bot_decision == "R":
+        print(f"Bot chose {bot_decision}")
+        print("You gained 1 point!")
+        score = score + 1
+        botpoints = botpoints - 1
+elif player_choice == "S":
+    if bot_decision == "R":
+        print("You lost 1 point")
+        score = score - 1
+        botpoints = botpoints + 1
+    elif bot_decision == "P":
+        print(f"Bot chose {bot_decision}")
+        print("You gained 1 point!")
+        score = score + 1
+        botpoints = botpoints - 1
+
+print(f"Your current score: {score}")
+
+player_choice = input("(R)ock (P)aper or (S)cissors?: ")
+bot_choices = ['R', 'P', 'S']
+print(f"RPS Bot chooses:")
+bot_decision = random.choice(bot_choices)
+time.sleep(0.5)
+
+if player_choice.lower == bot_decision:
+    print(f"Both selected {bot_decision}")
+
+elif player_choice == "R":
+    if bot_decision == "P":
+        print(f"Bot chose {bot_decision}")
+        print("You lost 1 point")
+        score = score - 1
+        botpoints = botpoints + 1
+    elif bot_decision == "S":
+        print(f"Bot chose {bot_decision}")
+        print("You gained 1 point!")
+        score = score + 1
+        botpoints = botpoints - 1
+elif player_choice == "P":
+    if bot_decision == "S":
+        print(f"Bot chose {bot_decision}")
+        print("You lost 1 point")
+        score = score - 1
+        botpoints = botpoints + 1
+    elif bot_decision == "R":
+        print(f"Bot chose {bot_decision}")
+        print("You gained 1 point!")
+        score = score + 1
+        botpoints = botpoints - 1
+elif player_choice == "S":
+    if bot_decision == "R":
+        print("You lost 1 point")
+        score = score - 1
+        botpoints = botpoints + 1
+    elif bot_decision == "P":
+        print(f"Bot chose {bot_decision}")
+        print("You gained 1 point!")
+        score = score + 1
+        botpoints = botpoints - 1
+
+print(f"Your current score: {score}")
+
+player_choice = input("(R)ock (P)aper or (S)cissors?: ")
+bot_choices = ['R', 'P', 'S']
+print(f"RPS Bot chooses:")
+bot_decision = random.choice(bot_choices)
+time.sleep(0.5)
+
+if player_choice.lower == bot_decision:
+    print(f"Both selected {bot_decision}")
+
+elif player_choice == "R":
+    if bot_decision == "P":
+        print(f"Bot chose {bot_decision}")
+        print("You lost 1 point")
+        score = score - 1
+        botpoints = botpoints + 1
+    elif bot_decision == "S":
+        print(f"Bot chose {bot_decision}")
+        print("You gained 1 point!")
+        score = score + 1
+        botpoints = botpoints - 1
+elif player_choice == "P":
+    if bot_decision == "S":
+        print(f"Bot chose {bot_decision}")
+        print("You lost 1 point")
+        score = score - 1
+        botpoints = botpoints + 1
+    elif bot_decision == "R":
+        print(f"Bot chose {bot_decision}")
+        print("You gained 1 point!")
+        score = score + 1
+        botpoints = botpoints - 1
+elif player_choice == "S":
+    if bot_decision == "R":
+        print("You lost 1 point")
+        score = score - 1
+        botpoints = botpoints + 1
+    elif bot_decision == "P":
+        print(f"Bot chose {bot_decision}")
+        print("You gained 1 point!")
+        score = score + 1
+        botpoints = botpoints - 1
+
+print(f"Your current score: {score}")
+
+player_choice = input("(R)ock (P)aper or (S)cissors?: ")
+bot_choices = ['R', 'P', 'S']
+print(f"RPS Bot chooses:")
+bot_decision = random.choice(bot_choices)
+time.sleep(0.5)
+
+if player_choice.lower == bot_decision:
+    print(f"Both selected {bot_decision}")
+
+elif player_choice == "R":
+    if bot_decision == "P":
+        print(f"Bot chose {bot_decision}")
+        print("You lost 1 point")
+        score = score - 1
+        botpoints = botpoints + 1
+    elif bot_decision == "S":
+        print(f"Bot chose {bot_decision}")
+        print("You gained 1 point!")
+        score = score + 1
+        botpoints = botpoints - 1
+elif player_choice == "P":
+    if bot_decision == "S":
+        print(f"Bot chose {bot_decision}")
+        print("You lost 1 point")
+        score = score - 1
+        botpoints = botpoints + 1
+    elif bot_decision == "R":
+        print(f"Bot chose {bot_decision}")
+        print("You gained 1 point!")
+        score = score + 1
+        botpoints = botpoints - 1
+elif player_choice == "S":
+    if bot_decision == "R":
+        print("You lost 1 point")
+        score = score - 1
+        botpoints = botpoints + 1
+    elif bot_decision == "P":
+        print(f"Bot chose {bot_decision}")
+        print("You gained 1 point!")
+        score = score + 1
+        botpoints = botpoints - 1
+
+print(f"Your current score: {score}")
+
+player_choice = input("(R)ock (P)aper or (S)cissors?: ")
+bot_choices = ['R', 'P', 'S']
+print(f"RPS Bot chooses:")
+bot_decision = random.choice(bot_choices)
+time.sleep(0.5)
+
+if player_choice.lower == bot_decision:
+    print(f"Both selected {bot_decision}")
+
+elif player_choice == "R":
+    if bot_decision == "P":
+        print(f"Bot chose {bot_decision}")
+        print("You lost 1 point")
+        score = score - 1
+        botpoints = botpoints + 1
+    elif bot_decision == "S":
+        print(f"Bot chose {bot_decision}")
+        print("You gained 1 point!")
+        score = score + 1
+        botpoints = botpoints - 1
+elif player_choice == "P":
+    if bot_decision == "S":
+        print(f"Bot chose {bot_decision}")
+        print("You lost 1 point")
+        score = score - 1
+        botpoints = botpoints + 1
+    elif bot_decision == "R":
+        print(f"Bot chose {bot_decision}")
+        print("You gained 1 point!")
+        score = score + 1
+        botpoints = botpoints - 1
+elif player_choice == "S":
+    if bot_decision == "R":
+        print("You lost 1 point")
+        score = score - 1
+        botpoints = botpoints + 1
+    elif bot_decision == "P":
+        print(f"Bot chose {bot_decision}")
+        print("You gained 1 point!")
+        score = score + 1
+        botpoints = botpoints - 1
+
+print(f"Your current score: {score}")
+
+player_choice = input("(R)ock (P)aper or (S)cissors?: ")
+bot_choices = ['R', 'P', 'S']
+print(f"RPS Bot chooses:")
+bot_decision = random.choice(bot_choices)
+time.sleep(0.5)
+
+if player_choice.lower == bot_decision:
+    print(f"Both selected {bot_decision}")
+
+elif player_choice == "R":
+    if bot_decision == "P":
+        print(f"Bot chose {bot_decision}")
+        print("You lost 1 point")
+        score = score - 1
+        botpoints = botpoints + 1
+    elif bot_decision == "S":
+        print(f"Bot chose {bot_decision}")
+        print("You gained 1 point!")
+        score = score + 1
+        botpoints = botpoints - 1
+elif player_choice == "P":
+    if bot_decision == "S":
+        print(f"Bot chose {bot_decision}")
+        print("You lost 1 point")
+        score = score - 1
+        botpoints = botpoints + 1
+    elif bot_decision == "R":
+        print(f"Bot chose {bot_decision}")
+        print("You gained 1 point!")
+        score = score + 1
+        botpoints = botpoints - 1
+elif player_choice == "S":
+    if bot_decision == "R":
+        print("You lost 1 point")
+        score = score - 1
+        botpoints = botpoints + 1
+    elif bot_decision == "P":
+        print(f"Bot chose {bot_decision}")
+        print("You gained 1 point!")
+        score = score + 1
+        botpoints = botpoints - 1
+
+print(f"Your current score: {score}")
+
+player_choice = input("(R)ock (P)aper or (S)cissors?: ")
+bot_choices = ['R', 'P', 'S']
+print(f"RPS Bot chooses:")
+bot_decision = random.choice(bot_choices)
+time.sleep(0.5)
+
+if player_choice.lower == bot_decision:
+    print(f"Both selected {bot_decision}")
+
+elif player_choice == "R":
+    if bot_decision == "P":
+        print(f"Bot chose {bot_decision}")
+        print("You lost 1 point")
+        score = score - 1
+        botpoints = botpoints + 1
+    elif bot_decision == "S":
+        print(f"Bot chose {bot_decision}")
+        print("You gained 1 point!")
+        score = score + 1
+        botpoints = botpoints - 1
+elif player_choice == "P":
+    if bot_decision == "S":
+        print(f"Bot chose {bot_decision}")
+        print("You lost 1 point")
+        score = score - 1
+        botpoints = botpoints + 1
+    elif bot_decision == "R":
+        print(f"Bot chose {bot_decision}")
+        print("You gained 1 point!")
+        score = score + 1
+        botpoints = botpoints - 1
+elif player_choice == "S":
+    if bot_decision == "R":
+        print("You lost 1 point")
+        score = score - 1
+        botpoints = botpoints + 1
+    elif bot_decision == "P":
+        print(f"Bot chose {bot_decision}")
+        print("You gained 1 point!")
+        score = score + 1
+        botpoints = botpoints - 1
+
+print(f"Your current score: {score}")
+
+player_choice = input("(R)ock (P)aper or (S)cissors?: ")
+bot_choices = ['R', 'P', 'S']
+print(f"RPS Bot chooses:")
+bot_decision = random.choice(bot_choices)
+time.sleep(0.5)
+
+if player_choice.lower == bot_decision:
+    print(f"Both selected {bot_decision}")
+
+elif player_choice == "R":
+    if bot_decision == "P":
+        print(f"Bot chose {bot_decision}")
+        print("You lost 1 point")
+        score = score - 1
+        botpoints = botpoints + 1
+    elif bot_decision == "S":
+        print(f"Bot chose {bot_decision}")
+        print("You gained 1 point!")
+        score = score + 1
+        botpoints = botpoints - 1
+elif player_choice == "P":
+    if bot_decision == "S":
+        print(f"Bot chose {bot_decision}")
+        print("You lost 1 point")
+        score = score - 1
+        botpoints = botpoints + 1
+    elif bot_decision == "R":
+        print(f"Bot chose {bot_decision}")
+        print("You gained 1 point!")
+        score = score + 1
+        botpoints = botpoints - 1
+elif player_choice == "S":
+    if bot_decision == "R":
+        print("You lost 1 point")
+        score = score - 1
+        botpoints = botpoints + 1
+    elif bot_decision == "P":
+        print(f"Bot chose {bot_decision}")
+        print("You gained 1 point!")
+        score = score + 1
+        botpoints = botpoints - 1
+
+print(f"Your current score: {score}")
+
+player_choice = input("(R)ock (P)aper or (S)cissors?: ")
+bot_choices = ['R', 'P', 'S']
+print(f"RPS Bot chooses:")
+bot_decision = random.choice(bot_choices)
+time.sleep(0.5)
+
+if player_choice.lower == bot_decision:
+    print(f"Both selected {bot_decision}")
+
+elif player_choice == "R":
+    if bot_decision == "P":
+        print(f"Bot chose {bot_decision}")
+        print("You lost 1 point")
+        score = score - 1
+        botpoints = botpoints + 1
+    elif bot_decision == "S":
+        print(f"Bot chose {bot_decision}")
+        print("You gained 1 point!")
+        score = score + 1
+        botpoints = botpoints - 1
+elif player_choice == "P":
+    if bot_decision == "S":
+        print(f"Bot chose {bot_decision}")
+        print("You lost 1 point")
+        score = score - 1
+        botpoints = botpoints + 1
+    elif bot_decision == "R":
+        print(f"Bot chose {bot_decision}")
+        print("You gained 1 point!")
+        score = score + 1
+        botpoints = botpoints - 1
+elif player_choice == "S":
+    if bot_decision == "R":
+        print("You lost 1 point")
+        score = score - 1
+        botpoints = botpoints + 1
+    elif bot_decision == "P":
+        print(f"Bot chose {bot_decision}")
+        print("You gained 1 point!")
+        score = score + 1
+        botpoints = botpoints - 1
+
+print("According to the points, The Winner is: ")
+if botpoints > score:
+    print(f"With {botpoints} points, The RPS bot won!")
+elif score > botpoints:
+    print(f"You! You had {score} points and the bot had {botpoints}!")
+elif score == botpoints:
+    print(f"Its a draw! Both got {score} points!")
+
+print("Restart game to play again")
+time.sleep(25)
